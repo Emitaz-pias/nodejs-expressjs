af691e2ec5b03e14c8cab4c40b3c25eb95fc0aa2
Emitaz-pias <61836021+Emitaz-pias@users.noreply.github.com>
2021-06-21 06:47:03 +0600

    pass json like data

--- index.js
+++ index.js
@@ -11,11 +11,18 @@ app.get("/adaenterprise", (req, res) =>
   res.send({ name: "Mr.Adabor Khan", occupation: " ship information" })
 );
 // how to pass  a data for a dynamic id
-app.get("/user/:id", (req, res) => {
+app.get("/user", (req, res) => {
   const id = req.params.id;
   const name = names[id];
   res.send(name);
   console.log(id);
+});
+
+// pass data like json
+app.get("/user/:id", (req, res) => {
+  const id = req.params.id;
+  const user = { id: id, name: names[id] };
+  res.send(user);
 });
 
 app.listen(8080, () => console.log("app listening  to port 8080"));
