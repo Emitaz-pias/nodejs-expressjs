d8ec718be9daa2073adacf742cf5b373852f5f2e
Emitaz-pias <61836021+Emitaz-pias@users.noreply.github.com>
2021-06-21 06:28:04 +0600

    pass data with static api

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 
-
 const app = express();
 
-app.get('/' ,(req,res)=>  res.send(" hello world!"))
+app.get("/", (req, res) => res.send(" hello world!"));
 
+app.get("/adaenterprise", (req, res) =>
+  res.send({ name: "Mr.Adabor Khan", occupation: " ship information" })
+);
 
-app.listen(8080,()=>console.log("app listening  to port 8080"))
+app.listen(8080, () => console.log("app listening  to port 8080"));
